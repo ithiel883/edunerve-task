40543c0dd1fbf7efa966adc76e67ffd37ecac027
Ajuka David <ajukadavid@Ezinne.local>
2021-10-14 14:19:04 +0100

    final commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3" style="font-weight: bold; color: white">© 2021 Copyright:
+    <a href="https://mdbootstrap.com/"> Edunerve Vue Project</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</template>
+
+<script>
+export default {
+ name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<nav class="navbar navbar-light bg-green-700" >
+  <div class="container">
+    <div class="text-center">
+     <a class="navbar-brand justify-content-center" href="#" >
+      <img src="../assets/logo.png" alt="..." height="36">
+    </a> 
+    </div>
+    
+   
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+
+<style>
+
+</style>
